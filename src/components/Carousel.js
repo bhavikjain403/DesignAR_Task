395c8c6f2d4eb48da4bcc8bb2f921e5dea298a41
Bhavik Jain <bhavikjain403@gmail.com>
2022-05-16 21:20:12 +0530

    Responsive Carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -8,6 +8,8 @@ export default function() {
     };
   
     return (
+      <div>
+      <div className='d-none d-md-block'>
       <Carousel autoPlay interval={2000} activeIndex={index} onSelect={handleSelect} indicators={false}>
         <Carousel.Item>
           <img
@@ -16,7 +18,7 @@ export default function() {
             alt="First slide"
           />
           <Carousel.Caption style={{height:"30%"}}>
-            <button style={{border:"none", background:"black", color:"white", width:"23%", height:"25%"}}>SHOP NOW</button>
+            <button style={{border:"none", background:"black", color:"white", width:"23%", height:"25%", fontSize:"75%"}}>SHOP NOW</button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -25,9 +27,8 @@ export default function() {
             src="https://ik.imagekit.io/thestylist/valaya/pub/media_valaya/HOMEPAGE_IMAGES/13_May_22/Desktop-Banner-1_(20).jpg"
             alt="Second slide"
           />
-  
           <Carousel.Caption style={{height:"30%"}}>
-            <button style={{border:"none", background:"black", color:"white", width:"23%", height:"25%"}}>SHOP NOW</button>
+            <button style={{border:"none", background:"black", color:"white", width:"23%", height:"25%", fontSize:"75%"}}>SHOP NOW</button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -36,9 +37,8 @@ export default function() {
             src="https://ik.imagekit.io/thestylist/valaya/pub/media_valaya/HOMEPAGE_IMAGES/13_May_22/Desktop-Banner-5.jpg"
             alt="Third slide"
           />
-  
           <Carousel.Caption style={{height:"30%"}}>
-            <button style={{border:"none", background:"black", color:"white", width:"23%", height:"25%"}}>SHOP NOW</button>
+            <button style={{border:"none", background:"black", color:"white", width:"23%", height:"25%", fontSize:"75%"}}>SHOP NOW</button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -47,9 +47,8 @@ export default function() {
             src="https://ik.imagekit.io/thestylist/valaya/pub/media_valaya/HOMEPAGE_IMAGES/13_May_22/Desktop-Banner-2_(16).jpg"
             alt="Third slide"
           />
-  
           <Carousel.Caption style={{height:"30%"}}>
-            <button style={{border:"none", background:"black", color:"white", width:"23%", height:"25%"}}>SHOP NOW</button>
+            <button style={{border:"none", background:"black", color:"white", width:"23%", height:"25%", fontSize:"75%"}}>SHOP NOW</button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -58,9 +57,8 @@ export default function() {
             src="https://ik.imagekit.io/thestylist/valaya/pub/media_valaya/HOMEPAGE_IMAGES/13_May_22/Desktop-Banner-1_(10).jpg"
             alt="Third slide"
           />
-  
           <Carousel.Caption style={{height:"30%"}}>
-            <button style={{border:"none", background:"black", color:"white", width:"23%", height:"25%"}}>SHOP NOW</button>
+            <button style={{border:"none", background:"black", color:"white", width:"23%", height:"25%", fontSize:"75%"}}>SHOP NOW</button>
           </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
@@ -69,11 +67,76 @@ export default function() {
             src="https://ik.imagekit.io/thestylist/valaya/pub/media_valaya/HOMEPAGE_IMAGES/13_May_22/Desktop-Banner-4_(1).jpg"
             alt="Third slide"
           />
-  
           <Carousel.Caption style={{height:"30%"}}>
-            <button style={{border:"none", background:"black", color:"white", width:"23%", height:"25%"}}>SHOP NOW</button>
+            <button style={{border:"none", background:"black", color:"white", width:"23%", height:"25%", fontSize:"75%"}}>SHOP NOW</button>
           </Carousel.Caption>
         </Carousel.Item>
       </Carousel>
+      </div>
+      <div className='d-md-none'>
+      <Carousel autoPlay interval={2000} activeIndex={index} onSelect={handleSelect} indicators={false}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://ik.imagekit.io/thestylist/valaya/pub/media_valaya/HOMEPAGE_IMAGES/13_May_22/Mobile-banner-3_(8).jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption style={{height:"30%"}}>
+            <button style={{border:"none", background:"black", color:"white", width:"50%", height:"60%", fontSize:"75%"}}>SHOP NOW</button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://ik.imagekit.io/thestylist/valaya/pub/media_valaya/HOMEPAGE_IMAGES/13_May_22/Mobile-banner-1_(22).jpg"
+            alt="Second slide"
+          />
+          <Carousel.Caption style={{height:"30%"}}>
+            <button style={{border:"none", background:"black", color:"white", width:"50%", height:"60%", fontSize:"75%"}}>SHOP NOW</button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://ik.imagekit.io/thestylist/valaya/pub/media_valaya/HOMEPAGE_IMAGES/14_May_22/Mobile-Banner_(13).jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption style={{height:"30%"}}>
+            <button style={{border:"none", background:"black", color:"white", width:"50%", height:"60%", fontSize:"75%"}}>SHOP NOW</button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://ik.imagekit.io/thestylist/valaya/pub/media_valaya/HOMEPAGE_IMAGES/13_May_22/Mobile-banner-2_(14).jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption style={{height:"30%"}}>
+            <button style={{border:"none", background:"black", color:"white", width:"50%", height:"60%", fontSize:"75%"}}>SHOP NOW</button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://ik.imagekit.io/thestylist/valaya/pub/media_valaya/HOMEPAGE_IMAGES/13_May_22/Mobile-Banner-1_(9).jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption style={{height:"30%"}}>
+            <button style={{border:"none", background:"black", color:"white", width:"50%", height:"60%", fontSize:"75%"}}>SHOP NOW</button>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://ik.imagekit.io/thestylist/valaya/pub/media_valaya/HOMEPAGE_IMAGES/13_May_22/Mobile-banner-6_(1).jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption style={{height:"30%"}}>
+            <button style={{border:"none", background:"black", color:"white", width:"50%", height:"60%", fontSize:"75%"}}>SHOP NOW</button>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+      </div>
+      </div>
     );
   }
